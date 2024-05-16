e809ba93bfd37cbec95c774520a918fede4fea0d
Akila De Silva <akiladesilva97@gmail.com>
2024-05-16 14:36:28 +0100

    springboot backend files added

---
+++ p2p-backend/src/test/java/com/p2p/p2pbackend/P2pBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p2p.p2pbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2pBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
