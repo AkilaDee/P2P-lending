6cf170ab9554c4185eeea9777b6e8514411250e2
Akila De Silva <akiladesilva97@gmail.com>
2024-05-16 01:49:35 +0100

    signin UI

--- p2p-frontend/src/App.js
+++ p2p-frontend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+// import {BrowserRouter as Router, Routes} from 'react-router-dom';
+import CustomerSignin from './components/customer/SignInCustomer';
+
+// import PrivateRoute from './components/HOC/PrivateRoute';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Router>
+        <Routes>
+
+        <PrivateRoute path="/Customer"  component={CustomerSignin} />
+            <Route exact path='/Customer' element={<PrivateRoute/>}>
+            <Route exact path='/Customer' element={<CustomerSignin/>}/>
+        </Route>
+
+        </Routes>
+      </Router>  */}
+      <CustomerSignin />
     </div>
   );
 }
