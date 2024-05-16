180e4eb3795cff749348fb6d2cc5176f014aa78b
Akila De Silva <akiladesilva97@gmail.com>
2024-05-16 20:08:20 +0100

    integrated postgres database and created user entity

---
+++ p2p-backend/src/main/java/com/p2p/p2pbackend/entity/User.java
@@ -0,0 +1,27 @@
+package com.p2p.p2pbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+}
